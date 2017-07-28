c2e2c752b04fd7f9c4981ba2c5bda9e4abc52c71
alon85 <alon.bulk@gmail.com>
2017-07-27 21:14:17 -0400

    adding utils and search query for title

--- config/default.js
+++ config/default.js
@@ -7,5 +7,9 @@ module.exports = {
                 password: 'al3185on1'
             }
         }
-    }
+    },
+    searchQuery: 'SELECT movieList \n' +
+    'FROM movies b \n' +
+    'UNNEST b.movieList \n' +
+    'WHERE lower(movieList.%s) like lower(%s)'
 };
--- routes/routes.js
+++ routes/routes.js
@@ -1,45 +1,37 @@
-// routes/calendarRouter.js
-
 var express = require('express');
 var logger = require('../lib/logger.js');
-var config = require('config');
-var json2csv = require('json2csv');
-var couchbaseClient = require('../lib/couchbase/client.js');
+var utils = require('../lib/utils.js');
 var router = express.Router();
 
 router.use(function (req, res, next) {
     if (!req.query.uid) return next('router');
     next()
-})
+});
 
 
 router.get('/search', function (req, res, next) {
-    var movieBucket = couchbaseClient.getBucket(config.couchbase.buckets['movies'].name);
-    var userId = req.query.uid;
-    movieBucket.get('uid_' + userId + '_list', function (err, result) {
+    utils.searchForMovies(req.query, function (result, err) {
         if (err) {
             res.status(500).json({'error': err});
+            next();
         }
-        movieList = result.value['movie_list'];
-        res.status(200).json(movieList);
+        res.status(200).json(result);
         next();
     });
 });
 
 router.get('/export', function (req, res, next) {
     var userId = req.query.uid;
-    movieBucket.get('uid_' + userId + '_list', function (err, result) {
+    utils.exportMovieList(userId, function (err, result) {
         if (err) {
             res.status(500).json({'error': err});
+            next();
         }
-        movieList = result.value['movie_list'];
-        var cellHeaders = ['Title', 'Format', 'Year'];
-        var csv = json2csv({data: result, fields: cellHeaders, del: ','});
+        ;
         res.set('Content-Type', 'text/csv; charset=utf-8');
-        res.status(200).send(new Buffer(csv));
+        res.status(200).send(new Buffer(result));
         next();
     });
-
 });
 
 module.exports = router;
