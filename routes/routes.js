24a99289c0289a6ee4b25bfc7fa86b46582a3dc8
alon85 <alon.bulk@gmail.com>
2017-12-29 09:52:03 -0500

    added remove for watchlist and movie list

--- routes/routes.js
+++ routes/routes.js
@@ -64,7 +64,7 @@ router.get('/movies/export', function (req, res, next) {
 
 router.post('/movies/add', function (req, res, next) {
     movieUtils.saveMovie(req.body.title, req.body.year, req.body.format, req.body.poster, req.body.uid).then(function (result) {
-        movieUtils.updateStats(req.body.format, req.body.year, req.body.uid).then(function (result) {
+        movieUtils.updateStats("add", req.body.format, req.body.year, req.body.uid).then(function (result) {
             res.status(200).json(result);
             next();
         }).catch(function (error) {
@@ -74,8 +74,28 @@ router.post('/movies/add', function (req, res, next) {
     });
 });
 
+router.post('/movies/remove', function (req, res, next) {
+    movieUtils.removeMovie(req.body.title, req.body.poster, req.body.uid).then(function (result) {
+        res.status(200).json(result);
+        next();
+    }).catch(function (error) {
+        res.status(500).json({'error': error});
+        next();
+    });
+});
+
 router.post('/movies/watchlist/add', function (req, res, next) {
     movieUtils.saveMovieToWatchlist(req.body.title, req.body.releaseDate, req.body.poster, req.body.uid).then(function (result) {
+        res.status(200).json(result);
+        next();
+    }).catch(function (error) {
+        res.status(500).json({'error': error});
+        next();
+    });
+});
+
+router.post('/movies/watchlist/remove', function (req, res, next) {
+    movieUtils.removeMovieFromWatchlist(req.body.title, req.body.uid).then(function (result) {
         res.status(200).json(result);
         next();
     }).catch(function (error) {
