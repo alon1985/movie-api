bb0227ad3971c4c28ef63c02b97c56814ecddf67
Alon Motro <alon@alons-mac-mini.lan>
2020-11-20 12:02:36 -0500

    digitalocean working simple

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
+const express = require('express')
+const router = express.Router();
+const app = express()
+const routes = require('./routes.js');
+const port = 3000
 
-'use strict';
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-var express = require('express');
-var app = express();
-var logger = require('./lib/logger.js');
-var compression = require('compression');
-var bodyParser = require('body-parser');
+app.use('/', routes);
 
-app.use(compression());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
-
-
-app.use(require('./routes/routes'));
-
-app.listen(8080, function () {
-    logger.info("listening on port 3000");
-});
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
