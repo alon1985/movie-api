05d5cab2755112425861c8638ded1b07901ae8ff
Alon <30555526+alon1985@users.noreply.github.com>
2020-11-28 11:29:49 -0500

    console logs

--- routes/movies.js
+++ routes/movies.js
@@ -8,12 +8,14 @@ const router = new Router();
 module.exports = router;
 
 router.get('/movies/:id', async (req, res) => {
+    console.log(`retrieving movie: ${req.params.id}`)
     const results = await processor.getMovie(req.params.id);
     return res.status(200).json(results);
 });
 
 
 router.get('/movies', async (req, res) => {
+    console.log(`retrieving all movies ${req.query}`);
     const results = await processor.getMovies(req.query);
     return res.status(200).json(results);
 });
@@ -47,6 +49,7 @@ router.delete('/watchlist/:id', async (req, res) => {
 })
 
 router.post('/movies/add', async (req, res) => {
+    console.log(`Adding movie: ${JSON.stringify(req.body, null, 2)}`)
     if(requestUtils.isRequestValid(req.body) && req.query.apiKey === process.env.API_KEY) {
         const results = await processor.saveMovieSeen(req.body.id, req.body.title, req.body.year, req.body.format, req.body.poster, req.body.plot);
         return res.status(200).json(results);
