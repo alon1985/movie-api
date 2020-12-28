4c6362e7cc4417ca4e3fa4f51bfafa05bb92593c
Alon <30555526+alon1985@users.noreply.github.com>
2020-12-28 13:08:46 -0500

    enable cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const mountRoutes = require('./routes');
 const port = 3000;
 
 app.use(express.json());;
 app.use(express.urlencoded({extended: true}));
+app.use(cors())
 
 
 mountRoutes(app);
